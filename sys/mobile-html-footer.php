e6afb4e90d2690e55c20f913004c1d37faf84ab7
Wohlstand <admin@wohlnet.ru>
2020-01-21 22:30:57 +0300

    Сделать "WohlstandFox" ссылкой на страницу "о проекте"

--- sys/mobile-html-footer.php
+++ sys/mobile-html-footer.php
@@ -1,4 +1,4 @@
-<p style="text-align: center;">© 2010-<?=date("Y");?> WohlstandFox</p>
+<p style="text-align: center;">© 2010-<?=date("Y");?> <a href="/m/about/">WohlstandFox</a></p>
 <p style="text-align: center;"><a href="/m/about/">О проекте</a><br/>
 <a href="/m/feedback/">Обратная связь</a><br/>
 <a href="/?full">полная версия</a></p>
