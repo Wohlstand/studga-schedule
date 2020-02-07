e73291baaaede2a7ec94c3c7f48bdaba1f7d8dc6
Wohlstand <admin@wohlnet.ru>
2020-02-07 16:20:11 +0300

    Лёгкая полировка формы обратной связи

--- sd/feedback/index.php
+++ sd/feedback/index.php
@@ -69,99 +69,112 @@ else
 
 
 <table align="center" cellpadding="0" style="width: 700; border-collapse: collapse">
-	<tr>
-		<td style="text-align: center; height: 37px">
-		<form  action="sendmail.php" method="post">
-			<br>Форма связи с системным администратором:<br>
-			<div class="loginpannel shedow">
-			<table cellpadding="0" style="width: 100%; border-collapse: collapse" cellspacing="4">
-				<tr>
-					<td style="width: 213px; text-align: right" valign="top">Ваше имя</td>
-					<td>
-					<input <?php if (($error[1]=="1")) echo "class='errorfield '";?>name="sendername" style="width: 340px" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[0])):"")?>"></td>
-				</tr>
-				<tr>
-					<td style="width: 213px; text-align: right" valign="top">Ваш электронный адрес</td>
-					<td><input <?php if (($error[2]=="1")||($error[3]=="1")) echo "class='errorfield '"; ?>name="mailfrom" style="width: 340px" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[1])):"")?>"></td>
-				</tr>
-				<tr>
-					<td style="width: 213px; text-align: right" valign="top">Ваше сообщение</td>
-					<td><textarea <?php if (($error[4]=="1")) echo "class='errorfield '";?>cols="54" rows="10" name="messg" style="width: 432px; height: 167px"><?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[2])):"")?></textarea></td>
-				</tr>
-				<tr style="display: none;">
-					<td style="width: 213px; text-align: right" valign="top">
-					    URL
-					</td>
-					<td>
-					    <input name="url" style="width: 340px" type="text" value="">
-					</td>
-				</tr>
-				<tr style="display: none;">
-					<td style="width: 213px; text-align: right" valign="top">
-					    Telephone
-					</td>
-					<td>
-					    <input name="phone" style="width: 340px" type="text" value="">
-					</td>
-				</tr>
-				<tr style="display: none;">
-					<td style="width: 213px; text-align: right" valign="top">
-					    Company
-					</td>
-					<td>
-					    <input name="company" style="width: 340px" type="text" value="">
-					</td>
-				</tr>
-				<tr>
-					<td style="width: 213px; text-align: right" valign="top"></td>
-			<td style="text-align: left">
-			<?php
-	            // show captcha HTML using Securimage::getCaptchaHtml()
-	            require_once dirname(__FILE__) . '/../securimage/securimage.php';
-	            $options = array();
-	            $options['input_name']             = 'wordofchuckenshit';
-	            $options['disable_flash_fallback'] = false; // allow flash fallback
+    <tr>
+        <td style="text-align: center; height: 37px">
+        <form  action="sendmail.php" method="post">
+            <br>Форма связи с разработчиком:<br>
+            <div class="loginpannel shedow">
+            <table cellpadding="0" style="width: 100%; border-collapse: collapse" cellspacing="4">
+                <tr>
+                    <td colspan="2">
+                        Прежде, чем задавать какой-либо вопрос, просим Вас обратить внимание на <a href="/about/faq">часто завадаемые вопросы</a>.
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        &nbsp;
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 213px; text-align: right" valign="top"><strong>Ваше имя</strong></td>
+                    <td>
+                    <input <?php if (($error[1]=="1")) echo "class='errorfield '";?>name="sendername" style="width: 340px" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[0])):"")?>"></td>
+                </tr>
+                <tr>
+                    <td style="width: 213px; text-align: right" valign="top">Ваш электронный адрес</td>
+                    <td><input <?php if (($error[2]=="1")||($error[3]=="1")) echo "class='errorfield '"; ?>name="mailfrom" style="width: 340px" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[1])):"")?>"></td>
+                </tr>
+                <tr>
+                    <td style="width: 213px; text-align: right" valign="top">
+                        <strong>Ваше сообщение</strong>
+                        <p>Просьба при сообщении об ошибках в расписании, обязательно указать группу и поток, где они были найдены.</p>
+                    </td>
+                    <td><textarea <?php if (($error[4]=="1")) echo "class='errorfield '";?>cols="54" rows="10" name="messg" style="width: 432px; height: 167px"><?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[2])):"")?></textarea></td>
+                </tr>
+                <tr style="display: none;">
+                    <td style="width: 213px; text-align: right" valign="top">
+                        URL
+                    </td>
+                    <td>
+                        <input name="url" style="width: 340px" type="text" value="">
+                    </td>
+                </tr>
+                <tr style="display: none;">
+                    <td style="width: 213px; text-align: right" valign="top">
+                        Telephone
+                    </td>
+                    <td>
+                        <input name="phone" style="width: 340px" type="text" value="">
+                    </td>
+                </tr>
+                <tr style="display: none;">
+                    <td style="width: 213px; text-align: right" valign="top">
+                        Company
+                    </td>
+                    <td>
+                        <input name="company" style="width: 340px" type="text" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width: 213px; text-align: right" valign="top"></td>
+            <td style="text-align: left">
+            <?php
+                // show captcha HTML using Securimage::getCaptchaHtml()
+                require_once dirname(__FILE__) . '/../securimage/securimage.php';
+                $options = array();
+                $options['input_name']             = 'wordofchuckenshit';
+                $options['disable_flash_fallback'] = false; // allow flash fallback
 
-	            if (!empty($_SESSION['ctform']['captcha_error'])) {
-		            // error html to show in captcha output
-		            $options['error_html'] = $_SESSION['ctform']['captcha_error'];
-	            }
+                if (!empty($_SESSION['ctform']['captcha_error'])) {
+                    // error html to show in captcha output
+                    $options['error_html'] = $_SESSION['ctform']['captcha_error'];
+                }
 
-	            echo "<div id='captcha_container_1'>\n";
-	            //echo Securimage::getCaptchaHtml($options);
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
-	            echo "<br>\nВведите текст на картинке:";
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
-	            echo "\n</div>\n";
+                echo "<div id='captcha_container_1'>\n";
+                //echo Securimage::getCaptchaHtml($options);
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
+                echo "<br>\nВведите текст на картинке:";
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
+                echo "\n</div>\n";
 
-	            /*
-	            // To render some or all captcha components individually
-	            $options['input_name'] = 'ct_captcha_2';
-	            $options['image_id']   = 'ct_captcha_2';
-	            $options['input_id']   = 'ct_captcha_2';
-	            $options['namespace']  = 'captcha2';
+                /*
+                // To render some or all captcha components individually
+                $options['input_name'] = 'ct_captcha_2';
+                $options['image_id']   = 'ct_captcha_2';
+                $options['input_id']   = 'ct_captcha_2';
+                $options['namespace']  = 'captcha2';
 
-	            echo "<br>\n<div id='captcha_container_2'>\n";
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
+                echo "<br>\n<div id='captcha_container_2'>\n";
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
 
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
 
-	            echo '<div style="clear: both"></div>';
+                echo '<div style="clear: both"></div>';
 
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT_LABEL);
-	            echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
-	            echo "\n</div>";
-	            */
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT_LABEL);
+                echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
+                echo "\n</div>";
+                */
             ?>
-			</tr>
-			</table></div>
-			<br>
-			<input name="Submit1" type="submit" value="Отправить сообщение"></form>
-		</td>
-	</tr>
+            </tr>
+            </table></div>
+            <br>
+            <input name="Submit1" type="submit" value="Отправить сообщение"></form>
+        </td>
+    </tr>
 </table>
 <script type="text/javascript"><!--
 captchareset();
--- sd/m/feedback/index.php
+++ sd/m/feedback/index.php
@@ -64,97 +64,116 @@ $formfields = "00000000000000000";
 </div>
 
 <table style="margin: auto auto; width: 95%; border-collapse: collapse">
-	<tr>
-		<td style="padding: 0px; text-align: center; height: 37px">
-		<form action="sendmail.php" method="post"><div>
-			<br/>Форма связи с системным администратором:<br/></div>
-			<div>
-			<table style="width: 100%; border-collapse: collapse">
-				<tr>
-					<td style="padding: 0px; width: 100%; text-align: left" valign="top">Ваше имя:<br/>
-					<input <?php if (($error[1]=="1")) echo "class='errorfield '";?>name="sendername" style="width: 100%" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[0])):"")?>"/></td>
-				</tr>
-				<tr>
-					<td style="padding: 0px; width: 100%; text-align: left" valign="top">Ваш электронный адрес<br/>
-					<input <?php if (($error[2]=="1")||($error[3]=="1")) echo "class='errorfield '";?>name="mailfrom" style="width: 100%" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[1])):"")?>"/></td>
-				</tr>
-				<tr>
-					<td style="padding: 0px; width: 100%; text-align: left" valign="top">Ваше сообщение<br/>
-					<textarea <?php if (($error[4]=="1")) echo "class='errorfield '";?>cols="54" rows="10" name="messg" style="width: 100%; height: 167px"><?=((!empty($_GET['formdata'])) ? htmlspecialchars(base64_decode($formfields[2])):"")?></textarea></td>
-				</tr>
-				<tr style="display: none;">
-					<td style="width: 213px; text-align: right" valign="top">
-					    URL
-					</td>
-					<td>
-					    <input name="url" style="width: 340px" type="text" value="">
-					</td>
-				</tr>
-				<tr style="display: none;">
-					<td style="width: 213px; text-align: right" valign="top">
-					    Telephone
-					</td>
-					<td>
-					    <input name="phone" style="width: 340px" type="text" value="">
-					</td>
-				</tr>
-				<tr style="display: none;">
-					<td style="width: 213px; text-align: right" valign="top">
-					    Company
-					</td>
-					<td>
-					    <input name="company" style="width: 340px" type="text" value="">
-					</td>
-				</tr>
-				<tr>
-				<td style="padding: 0px; width: 100%; text-align: center" valign="top">
+    <tr>
+        <td style="padding: 0px; text-align: center; height: 37px">
+        <form action="sendmail.php" method="post">
+            <div>
+                <br/>Форма связи с разработчиком:<br/><br/>
+                <br/>
+            <div>
+            <table style="width: 100%; border-collapse: collapse">
+                <tr>
+                    <td>
+                        Прежде, чем задавать какой-либо вопрос, просим Вас обратить внимание на <a href="/m/about/faq">часто завадаемые вопросы</a>.
+                        <br/>
+                        <br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0px; width: 100%; text-align: left" valign="top">
+                        Ваше имя:<br/>
+                        <input <?php if (($error[1]=="1")) echo "class='errorfield '";?>name="sendername" style="width: 100%" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[0])):"")?>"/>
+                        <br/>
+                        <br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0px; width: 100%; text-align: left" valign="top">
+                        Ваш электронный адрес<br/>
+                        <input <?php if (($error[2]=="1")||($error[3]=="1")) echo "class='errorfield '";?>name="mailfrom" style="width: 100%" type="text" value="<?=((!empty($_GET['formdata']))?htmlspecialchars(base64_decode($formfields[1])):"")?>"/>
+                        <br/>
+                        <br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 0px; width: 100%; text-align: left" valign="top">
+                        <strong>Ваше сообщение</strong>
+                        <p>Просьба при сообщении об ошибках в расписании, обязательно указать группу и поток, где они были найдены.</p>
+                    <textarea <?php if (($error[4]=="1")) echo "class='errorfield '";?>cols="54" rows="10" name="messg" style="width: 100%; height: 167px"><?=((!empty($_GET['formdata'])) ? htmlspecialchars(base64_decode($formfields[2])):"")?></textarea></td>
+                </tr>
+                <tr style="display: none;">
+                    <td style="width: 213px; text-align: right" valign="top">
+                        URL
+                    </td>
+                    <td>
+                        <input name="url" style="width: 340px" type="text" value="">
+                    </td>
+                </tr>
+                <tr style="display: none;">
+                    <td style="width: 213px; text-align: right" valign="top">
+                        Telephone
+                    </td>
+                    <td>
+                        <input name="phone" style="width: 340px" type="text" value="">
+                    </td>
+                </tr>
+                <tr style="display: none;">
+                    <td style="width: 213px; text-align: right" valign="top">
+                        Company
+                    </td>
+                    <td>
+                        <input name="company" style="width: 340px" type="text" value="">
+                    </td>
+                </tr>
+                <tr>
+                <td style="padding: 0px; width: 100%; text-align: center" valign="top">
                 <?php
-	                // show captcha HTML using Securimage::getCaptchaHtml()
-	                require_once dirname(__FILE__) . '/../../securimage/securimage.php';
-	                $options = array();
-	                $options['input_name']             = 'wordofchuckenshit';
-	                $options['disable_flash_fallback'] = false; // allow flash fallback
+                    // show captcha HTML using Securimage::getCaptchaHtml()
+                    require_once dirname(__FILE__) . '/../../securimage/securimage.php';
+                    $options = array();
+                    $options['input_name']             = 'wordofchuckenshit';
+                    $options['disable_flash_fallback'] = false; // allow flash fallback
 
-	                if (!empty($_SESSION['ctform']['captcha_error'])) {
-		                // error html to show in captcha output
-		                $options['error_html'] = $_SESSION['ctform']['captcha_error'];
-	                }
+                    if (!empty($_SESSION['ctform']['captcha_error'])) {
+                        // error html to show in captcha output
+                        $options['error_html'] = $_SESSION['ctform']['captcha_error'];
+                    }
 
-	                echo "<div id='captcha_container_1'>\n";
-	                //echo Securimage::getCaptchaHtml($options);
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
-	                echo "<br>\nВведите текст на картинке:";
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
-	                echo "\n</div>\n";
+                    echo "<div id='captcha_container_1'>\n";
+                    //echo Securimage::getCaptchaHtml($options);
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
+                    echo "<br>\nВведите текст на картинке:";
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
+                    echo "\n</div>\n";
 
-	                /*
-	                // To render some or all captcha components individually
-	                $options['input_name'] = 'ct_captcha_2';
-	                $options['image_id']   = 'ct_captcha_2';
-	                $options['input_id']   = 'ct_captcha_2';
-	                $options['namespace']  = 'captcha2';
+                    /*
+                    // To render some or all captcha components individually
+                    $options['input_name'] = 'ct_captcha_2';
+                    $options['image_id']   = 'ct_captcha_2';
+                    $options['input_id']   = 'ct_captcha_2';
+                    $options['namespace']  = 'captcha2';
 
-	                echo "<br>\n<div id='captcha_container_2'>\n";
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
+                    echo "<br>\n<div id='captcha_container_2'>\n";
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_IMG);
 
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_ICON_REFRESH);
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_AUDIO);
 
-	                echo '<div style="clear: both"></div>';
+                    echo '<div style="clear: both"></div>';
 
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT_LABEL);
-	                echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
-	                echo "\n</div>";
-	                */
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT_LABEL);
+                    echo Securimage::getCaptchaHtml($options, Securimage::HTML_INPUT);
+                    echo "\n</div>";
+                    */
                 ?>
                 </tr>
-			</table>
-			<br/>
-			<input name="Submit1" type="submit" value="Отправить сообщение"/></div></form>
-		</td>
-	</tr>
+            </table>
+            <br/>
+            <input name="Submit1" type="submit" value="Отправить сообщение"/></div></form>
+        </td>
+    </tr>
 </table>
 <script type="text/javascript"><!--
 captchareset();
